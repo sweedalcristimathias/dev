5326350438eebb3811d9a476a50098b0716315f6
sweedalcristimathias <151523676+sweedalcristimathias@users.noreply.github.com>
2024-03-27 15:44:01 +0530

    Update booktest.py

--- pythonproblems/tests/booktest.py
+++ pythonproblems/tests/booktest.py
@@ -7,7 +7,8 @@ import sys
 
 # Import the functions to be tested
 sys.path.insert(1,r'pythonproblems/books/books.py')
-from ..books import load_book_data, search_books_by_year, get_publication_year
+from pythonproblems.books import load_book_data, search_books_by_year, get_publication_year
+
 
 class TestBookFunctions(unittest.TestCase):
     def setUp(self):
