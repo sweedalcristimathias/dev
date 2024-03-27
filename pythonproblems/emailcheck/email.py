2ffe4f805009608c88b467aa5adc9491b70b4d66
sweedalcristimathias <151523676+sweedalcristimathias@users.noreply.github.com>
2024-03-27 12:41:22 +0530

    Update email.py

--- pythonproblems/emailcheck/email.py
+++ pythonproblems/emailcheck/email.py
@@ -1,11 +1,16 @@
 import re
+import sys
 
 def valid_email(email):
     pattern = r'^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,}$'
     return re.match(pattern, email) is not None
 
 def main():
-    email = input("Enter an email address: ")
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <email>")
+        sys.exit(1)
+
+    email = sys.argv[1]
     if valid_email(email):
         print("Valid email address.")
     else:
@@ -13,3 +18,32 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import re
+
+# def valid_email(email):
+#     pattern = r'^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,}$'
+#     return re.match(pattern, email) is not None
+
+# def main():
+#     email = input("Enter an email address: ")
+#     if valid_email(email):
+#         print("Valid email address.")
+#     else:
+#         print("Invalid email address.")
+
+# if __name__ == "__main__":
+#     main()
